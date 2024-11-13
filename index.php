487ea5804807ecb42cec60b2a100f34a44285504
sasi maelani <sasimaelani7@gmail.com>
2024-11-13 21:57:22 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h2>Selamat Datang di Toko Alat Sekolah Teh Melani</h2>
+    <a href="view_data.php">Lihat Data Barang</a>
+</body>
+</html>
